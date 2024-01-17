dd7f032c9ee1821618f73effad85ea9305e22b97
Jesse Rosengrant <jrosengrant616@gmail.com>
2024-01-16 21:41:37 -0500

    one little change

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '../public/vite.svg';
-import './App.css';
-import * as api from './backend/api.js';
-
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <div>
-        {' '}
-        <img src="https://www.artic.edu/iiif/2/e966799b-97ee-1cc6-bd2f-a94b4b8bb8f9/full/843,/0/default.jpg"></img>
-      </div>
-    </>
-=======
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignupPage from './pages/Signup';
@@ -43,18 +5,17 @@ import LoginPage from '../pages/Login';
 
 function App() {
   return (
-    <div className='min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8'>
-      <div className='max-w-md w-full space-y-8'>
+    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
         <BrowserRouter>
           <Routes>
-            <Route path='/' element={<LoginPage />} />
-            <Route path='/signup' element={<SignupPage />} />
-            <Route path='/home' element={<HomePage />} />
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/home" element={<HomePage />} />
           </Routes>
         </BrowserRouter>
       </div>
     </div>
->>>>>>> origin/frontend
   );
 }
 
